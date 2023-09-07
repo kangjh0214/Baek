8b836e2c32d829c59645d6cbfb2ba20cf0ab06ae
KK <kangjh2006@gmail.com>
2023-09-08 08:42:58 +0900

    2588번

---
+++ Baekk/2588번.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << a * (b % 10) << "\n";
+    std::cout << a * (b % 100 / 10) << "\n";
+    std::cout << a * (b / 100) << "\n";
+    std::cout << a * b << "\n";
+}
