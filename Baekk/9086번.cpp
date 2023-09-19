2ed5992ea0b4dabbed50ebfda421275532a1e283
KK <kangjh2006@gmail.com>
2023-09-20 08:45:35 +0900

    Create 9086번.cpp

---
+++ Baekk/9086번.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n;
+    string k;
+    cin >> n;
+    while (n--)
+    {
+        cin >> k;
+        cout << k[0] << k[k.size() - 1] << '\n';
+    }
+}
