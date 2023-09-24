b2d563be2b1fbc3f7d9a05898d8449022e6d8d50
KK <kangjh2006@gmail.com>
2023-09-25 08:40:22 +0900

    Create 10430번.cpp

---
+++ Baekk/10430번.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+
+    cout << (A + B) % C << endl;
+    cout << ((A % C) + (B % C)) % C << endl;
+    cout << (A * B) % C << endl;
+    cout << ((A % C) * (B % C)) % C << endl;
+}
