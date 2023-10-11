3f2f830ed6c2dd61bfe670fc7df94fe88db27e5e
KK <kangjh2006@gmail.com>
2023-10-12 08:49:36 +0900

    Create 10951번.cpp

---
+++ Baekk/10951번.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	while (!(cin >> a >> b).eof())
+	{	// 혹은 eof() 대신 fail()을 사용해도 된다.
+		cout << a + b << "\n";
+	}
+
+	return 0;
+}
