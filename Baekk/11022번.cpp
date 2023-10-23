2dce5fa6ffc98b97da1b6b3efc2c4e18ddfef0c0
KK <kangjh2006@gmail.com>
2023-10-24 08:33:59 +0900

    Create 11022번.cpp

---
+++ Baekk/11022번.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, a, b;
+
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << endl;
+    }
+}
